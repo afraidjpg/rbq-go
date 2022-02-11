952ef8b09ba9fa8b6871f19f073eaa82e719b5ec
afraid.jpg <578213059@qq.com>
2022-02-11 17:04:38 +0800

    add leveldb support

---
+++ core/app/leveldb.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"github.com/syndtr/goleveldb/leveldb"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"qq-robot-go/core/config"
+)
+
+var storage *leveldb.DB
+
+// 连接到leveldb文件
+func connectToLeveldb() {
+	if storage != nil {
+		return
+	}
+
+	basePath, _ := os.Getwd()
+	loginQQ := config.Cfg.GetInt64("account.login_qq")
+	dataPath := filepath.Join(basePath, fmt.Sprintf("../../data/storage_%d", loginQQ))
+	if _, err:= os.Stat("./leveldb.go"); errors.Is(err, fs.ErrNotExist){
+		dataPath = filepath.Join(basePath, fmt.Sprintf("./data/storage_%d", loginQQ))
+	}
+
+	db, err := leveldb.OpenFile(dataPath, nil)
+	if err != nil {
+		log.Fatal("初始化数据库出错，原因: ", err)
+	}
+	storage = db
+}
+
+func GetStorage() *leveldb.DB {
+	if storage == nil {
+		connectToLeveldb()
+	}
+	return storage
+}
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/robfig/cron/v3 v3.0.0 // indirect
 	github.com/spf13/viper v1.10.1 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 )
