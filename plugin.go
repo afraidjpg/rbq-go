e8281b047b24fa83b5de161e22a7103a08d7e0ae
afraid.jpg <zk991020@gmail.com>
2023-01-12 19:02:24 +0800

    update readme && comment

--- plugin.go
+++ plugin.go
@@ -9,9 +9,10 @@ import (
 	"strings"
 )
 
-type PluginFunc func(*Context)
+type PluginFunc func(*Context) //
 type PluginFilterFunc func(ctx *Context) bool
 
+// PluginGroupOption 插件组选项
 type PluginGroupOption struct {
 	GPluginOpt *PluginOption // 组内插件的默认设置，详情见 PluginOption 的注释说明
 }
